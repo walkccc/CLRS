d0e838ca4ab9a9f1bee6ba7f63134a95d698fd71
Jay <walkccray@gmail.com>
2018-03-20 21:47:45 +0800

    Initialize CLRS

---
+++ docs/js/katex.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    renderMathInElement(document.body, {
+        delimiters: [
+            {left: "$$", right: "$$", display: true},
+            {left: "$", right: "$", display: false}
+        ]
+    });
+});
